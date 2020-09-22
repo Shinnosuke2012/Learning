bed7604c6f6abbe9bf35518a6b5e3a59c7895082
Shinnosuke2012 <71538983+Shinnosuke2012@users.noreply.github.com>
2020-09-23 08:53:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 $(function() {
 
-  $('.lesson-hover').hover(
-      function() {
-        // 子要素の「.text-contents」の要素を取得し、text-activeクラスをつけてください
-        $(this).find('.txt-contents').addClass('text-active');
+$('.menu-icon').click(function(){
+  $('.header-small').fadeIn();
+});
 
-      },
-      function() {
-        // 子要素の「.text-contents」の要素を取得し、text-activeクラスを外してください
-        $(this).find('.txt-contents').removeClass('text-active');
+$('.menu-icon').click(function(){
+  $('.header-small').fadeOut();
+});  
 
-      }
-    );
 });
